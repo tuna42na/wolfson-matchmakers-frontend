fc4fe2cfbc8342634bd98d4b90f9d3d97eeabcdb
Tuna Fortuna <tuna42na@gmail.com>
2020-11-10 20:52:06 -0500

    Heroku Package

--- src/components/matches.js
+++ src/components/matches.js
@@ -1,14 +1,37 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { TransitionGroup, CSSTransition } from "react-transition-group";
 import { Heading } from "@chakra-ui/core";
+import axios from "axios";
+import { useDispatch } from "react-redux";
 
 const Matches = () => {
+  const [matches, setMatches] = useState([{ name: "" }]);
+
+  useEffect(() => {
+    const fetchPeople = async () => {
+      const students = await axios("src/data/people.json");
+      setMatches(students.data);
+    };
+    fetchPeople();
+  }, []);
+
+  console.log(matches);
   return (
     <>
       <Heading id="header-font" as="h1" fontSize="7vh">
         Matches for "Your Name"
       </Heading>
-      <button> Coming Soon! </button>
+      <div>
+        {matches === [] ? (
+          <span>"Loading..."</span>
+        ) : (
+          <span>
+            {matches.data.map((item) => (
+              <span>{item.name}</span>
+            ))}
+          </span>
+        )}
+      </div>
     </>
   );
 };
